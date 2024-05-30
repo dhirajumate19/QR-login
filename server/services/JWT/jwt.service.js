2436073fc41ef27b3569ca5683b2aff1cd1c777b
Dhiraj Umate <dhiraju1999@gmail.com>
2024-05-30 07:20:00 +0530

    debug some code in jwt service

--- server/services/JWT/jwt.service.js
+++ server/services/JWT/jwt.service.js
@@ -1,13 +1,42 @@
 import jwt from "jsonwebtoken";
 import { ACCESS_EXP_IN, JWT_PWD, REFRESH_EXP_IN } from "./jwt.config.js";
+import { userTokenModel } from "./jwt.model.js";
 
 // Function to generate access and refresh tokens
-export const generateToken = (payload) => {
-  const accessToken = jwt.sign(payload, JWT_PWD, { expiresIn: ACCESS_EXP_IN });
-  const refreshToken = jwt.sign(payload, JWT_PWD, {
-    expiresIn: REFRESH_EXP_IN,
-  });
-  return { accessToken, refreshToken };
+export const generateToken = async (payload) => {
+  try {
+    if (!payload.userId) {
+      throw new Error("Missing required Enviornment Variable");
+    }
+    const accessToken = jwt.sign(payload, JWT_PWD, {
+      expiresIn: ACCESS_EXP_IN,
+    });
+    const refreshToken = jwt.sign(payload, JWT_PWD, {
+      expiresIn: REFRESH_EXP_IN,
+    });
+
+    const userTokenData = await userTokenModel.findOne({
+      userId: payload.userId,
+    });
+    if (!userTokenData) {
+      const newUserTokenData = {
+        userId: payload.userId,
+        accessToken: [accessToken],
+        refreshToken: [refreshToken],
+      };
+      const userToken = new userTokenModel(newUserTokenData);
+      await userToken.save();
+      return { accessToken, refreshToken };
+    }
+
+    userTokenData.accessToken.push(accessToken);
+    userTokenData.refreshToken.push(refreshToken);
+    await userTokenData.save();
+    return { accessToken, refreshToken };
+  } catch (error) {
+    console.log("Error while generating token:", error);
+    return { error: "Error while generating token" };
+  }
 };
 
 // Function to validate a token
